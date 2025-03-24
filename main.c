d6d8fba98ce8543caf67e8b4a9c9d4566e85d54b
Mateusz <410372@student.agh.edu.pl>
2025-03-24 01:23:39 +0100

    Small changes, need more polish

--- main.c
+++ main.c
@@ -35,24 +35,25 @@ typedef struct {
     int stepCount;
 } Intersection;
 
-void initializeIntersection(volatile Intersection *intersection){
+void initializeIntersection(Intersection *intersection){
+    intersection->stepCount = 0;
     for(int i=0;i<4;i++){
         intersection->roads[i].lightState = RED;
         intersection->roads[i].vehicleCount = 0;
     }
 }
 
-void addVehicle(volatile Intersection *intersection, const char *vehicleId, RoadDirection startRoad, RoadDirection endRoad){
-    Road road = intersection->roads[startRoad];
-    if(road.vehicleCount >= MAX_VEHICLES){
+void addVehicle(Intersection *intersection, const char *vehicleId, RoadDirection startRoad, RoadDirection endRoad){
+    Road *road = &intersection->roads[startRoad];
+    if(road->vehicleCount >= MAX_VEHICLES){
         perror("Unable to add more cars to this road!\n");
         return;
     }
 
-    strcpy(road.vehicles[road.vehicleCount].vehicleId,vehicleId);
-    road.vehicles[road.vehicleCount].startRoad = startRoad;
-    road.vehicles[road.vehicleCount].endRoad = endRoad;
-    road.vehicleCount += 1;
+    strcpy(road->vehicles[road->vehicleCount].vehicleId,vehicleId);
+    road->vehicles[road->vehicleCount].startRoad = startRoad;
+    road->vehicles[road->vehicleCount].endRoad = endRoad;
+    road->vehicleCount += 1;
 }
 
 void step(Intersection *intersection, FILE *output) {
@@ -63,59 +64,16 @@ void step(Intersection *intersection, FILE *output) {
 
         if (road->lightState == GREEN && road->vehicleCount > 0) {
             Vehicle exitingVehicle = road->vehicles[0];
-            RoadDirection start = exitingVehicle.startRoad;
-            RoadDirection end = exitingVehicle.endRoad;
-
-            // Identify conflicting roads
-            Road *oppositeRoad = &intersection->roads[(i + 2) % 4]; // Opposing road
-            Road *leftRoad = &intersection->roads[(i + 1) % 4]; // Road to left
-            Road *rightRoad = &intersection->roads[(i + 3) % 4]; // Road to right
-
-            bool canMove = true;
-
-            if (end == (i + 2) % 4) { // Going straight
-                if (oppositeRoad->lightState == GREEN && oppositeRoad->vehicleCount > 0) {
-                    Vehicle opposingVehicle = oppositeRoad->vehicles[0];
-                    if (opposingVehicle.endRoad == start) { 
-                        canMove = false; // Collision risk
-                        fprintf(output, "Vehicle %s cannot move: conflict with vehicle %s going straight\n",
-                                exitingVehicle.vehicleId, opposingVehicle.vehicleId);
-                    }
-                }
-            } else if (end == (i + 1) % 4) { // Turning left
-                if (oppositeRoad->lightState == GREEN && oppositeRoad->vehicleCount > 0) {
-                    Vehicle opposingVehicle = oppositeRoad->vehicles[0];
-                    if (opposingVehicle.endRoad == (i + 2) % 4) { 
-                        canMove = false; // Left turn yields
-                        fprintf(output, "Vehicle %s must wait: yielding to vehicle %s going straight\n",
-                                exitingVehicle.vehicleId, opposingVehicle.vehicleId);
-                    }
-                }
-            } else if (end == (i + 3) % 4) { // Turning right
-                if (rightRoad->lightState == GREEN && rightRoad->vehicleCount > 0) {
-                    Vehicle rightVehicle = rightRoad->vehicles[0];
-                    if (rightVehicle.endRoad == start) { 
-                        canMove = false; // Right turn blocked
-                        fprintf(output, "Vehicle %s cannot turn right: blocked by vehicle %s\n",
-                                exitingVehicle.vehicleId, rightVehicle.vehicleId);
-                    }
-                }
+            fprintf(output, "%s\n", exitingVehicle.vehicleId);
+            for(int j=1;j<road->vehicleCount;j++){
+                road->vehicles[j-1] = road->vehicles[j];
             }
-
-            if (canMove) {
-                fprintf(output, "Vehicle %s moved from %d to %d\n", exitingVehicle.vehicleId, start, end);
-
-                // Remove vehicle from queue
-                for (int j = 1; j < road->vehicleCount; j++) {
-                    road->vehicles[j - 1] = road->vehicles[j];
-                }
-                road->vehicleCount--;
-            }
+            road->vehicleCount -= 1;
         }
     }
 
     // Change lights every 10 steps
-    int nextLight = (intersection->stepCount / 10) % 4;
+    int nextLight = (intersection->stepCount / 2) % 4;
     for (int i = 0; i < 4; i++) {
         intersection->roads[i].lightState = (i == nextLight) ? GREEN : RED;
     }
@@ -123,13 +81,20 @@ void step(Intersection *intersection, FILE *output) {
     intersection->stepCount++;
 }
 int main(int argc, char *argv[]){
-    if (argc != 3) {
+    /*if (argc != 3) {
         fprintf(stderr, "Usage: %s <input_file> <output_file>\n", argv[0]);
         return 1;
-    }
+    }*/
 
-    const char *inputFile = argv[1];
-    const char *outputFile = argv[2];
+    //const char *inputFile = argv[1];
+    //const char *outputFile = argv[2];
+    const char *inputFile = "c_input.txt";
+    const char *outputFile = "c_output.txt";
+
+    char command[20];
+    char vehicleId[20];
+    RoadDirection startRoad;
+    RoadDirection endRoad;
 
     FILE *input = fopen(inputFile, "r");
     if (!input) {
@@ -146,5 +111,17 @@ int main(int argc, char *argv[]){
 
     Intersection intersection;
     initializeIntersection(&intersection);
-    printf("%d\n",intersection.roads[SOUTH].lightState);
+    
+
+    while (fscanf(input, "%s", command) != EOF){
+        if(strcmp(command, "addVehicle") == 0){
+            fscanf(input, "%s %d %d", vehicleId, (int*)&startRoad, (int*)&endRoad);
+            addVehicle(&intersection, vehicleId, startRoad, endRoad);
+        } else if (strcmp(command, "step") == 0) {
+            step(&intersection, output);
+        }
+    }
+
+    free(input);
+    free(output);
 }
---
+++ parser.py
@@ -0,0 +1,52 @@
+import json
+import subprocess
+
+def sideToEnum(side):
+    dictionary = {"north": 0, "south": 1, "east": 2, "west": 3}
+    return dictionary[side]
+
+def parse_json_input(input_file):
+    with open(input_file, 'r') as f:
+        data = json.load(f)
+    return data
+
+def generate_c_input(json_data, c_input_file):
+    with open(c_input_file, 'w') as f:
+        for command in json_data['commands']:
+            if command['type'] == 'addVehicle':
+                f.write(f"addVehicle {command['vehicleId']} {sideToEnum(command['startRoad'])} {sideToEnum(command['endRoad'])}\n")
+            elif command['type'] == 'step':
+                f.write("step\n")
+
+def parse_c_output(c_output_file):
+    step_statuses = []
+    with open(c_output_file, 'r') as f:
+        for line in f:
+            if line.startswith("stepStatus:"):
+                step_status = json.loads(line[len("stepStatus: "):])
+                step_statuses.append(step_status)
+    return step_statuses
+
+def main(input_file, output_file):
+    json_data = parse_json_input(input_file)
+
+    c_input_file = 'c_input.txt'
+    generate_c_input(json_data, c_input_file)
+
+    c_output_file = 'c_output.txt'
+    #subprocess.run(["./main", c_input_file, c_output_file], check=True)
+
+    #step_statuses = parse_c_output(c_output_file)
+
+    #with open(output_file, 'w') as f:
+        #json.dump({"stepStatuses": step_statuses}, f, indent=2)
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) != 3:
+        print("Usage: python3 parser.py <input_file> <output_file>")
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+    main(input_file, output_file)
